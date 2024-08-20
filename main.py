611ec3f407efc2bec84e333da775d7d02ae32b45
ATA AVCI <129445221+ataavci@users.noreply.github.com>
2024-08-20 21:00:45 +0300

    deneme2

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-deneme
+import numpy as np
+import pandas as pd
+from openpyxl import load_workbook
+import xlsxwriter
+import os
+
+
+
+def dosya_erisim(path, sheet_name, col):
+    
+    workbook =load_workbook(filename=path)
+    if sheet_name not in workbook.sheetnames:
+        print("HATALI SHEET NAME")
+        return
+    sheet = workbook[sheet_name]
+    for cell in sheet[col]:
+        print(f"{cell.column_letter}{cell.row}={cell.value}")
+
+if __name__ == "__main__":
+    dosya_erisim("ets_hesap.xlsx", sheet_name="ETS CONTENT", col=["A","B"])
